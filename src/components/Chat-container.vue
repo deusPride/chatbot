f5e2e9cafa602a0b3a061c7aa72964f4d983aaab
deuspride <sinpride9@gmail.com>
2024-01-22 12:17:03 +0100

    Init Integrable ChatBot

---
+++ src/components/Chat-container.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <ChatMessage />
+    <ChatMessage />
+  </div>
+</template>
+
+<script>
+import ChatMessage from "./Chat/Message.vue";
+export default {
+  name: "ChatContainer",
+  components: { ChatMessage },
+  data() {
+    const date = new Date();
+
+    return {
+      title: "Conversationnal ChatBot",
+      sender: "Bot",
+      content: "test",
+      date: date.toLocaleDateString(),
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  height: 800px;
+  width: 600px;
+  margin: auto;
+  border: 1px solid #111f2c;
+}
+.hello h1 {
+  margin-top: 0px;
+  background-color: #42b983;
+  color: beige;
+  padding: 0 2%;
+  border-bottom: 1px solid #111f2c;
+}
+</style>
---
+++ src/components/Chat/Message.vue
@@ -0,0 +1,47 @@
+<template >
+<div class="message">
+<h5>{{ sender }}</h5>
+<p>{{ content }}</p>
+<span>{{ date }}</span>
+</div>
+</template>
+<script>
+export default {
+    name:'ChatMessage',
+    props: [
+        'text', // Content of the message
+        'author', // Author of the message
+        'dark' // Background 
+    ],
+    data(){
+        return{
+            sender: 'Bot',
+            content: 'test'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.message{
+    margin-top: 3px;
+    padding: 5px;
+    color: black;
+    bottom: 2px;
+    position: relative;
+    border: 1px solid #111f2c;
+    max-width: 50%;
+
+}
+.message.bot{
+    background-color: beige;
+    left: 5px;
+}
+.message.user{
+background-color: rgb(53, 219, 95);
+    right: 5px;
+}
+p,h5{
+    margin: 2px ;
+}
+</style>
